1a66fd8ad0795a60aecae59d7957f106d56afc45
Luong <luong.pdm2204@gmail.com>
2024-01-06 11:31:16 +0700

    get image for correct userId

--- src/main/java/vn/edu/usth/controller/ImageController.java
+++ src/main/java/vn/edu/usth/controller/ImageController.java
@@ -1,22 +1,18 @@
 package vn.edu.usth.controller;
 
-import io.quarkus.logging.Log;
-import jakarta.annotation.security.PermitAll;
 import jakarta.annotation.security.RolesAllowed;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
-import jakarta.ws.rs.core.StreamingOutput;
 import org.eclipse.microprofile.openapi.annotations.enums.SecuritySchemeType;
 import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
 import org.eclipse.microprofile.openapi.annotations.security.SecurityScheme;
 import vn.edu.usth.payload.ImageRGBFromHyper;
-import vn.edu.usth.payload.ViewMap;
 import vn.edu.usth.service.image.ImageService;
 
-import java.io.*;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
@@ -36,14 +32,18 @@ public class ImageController {
     @POST
     @RolesAllowed({"USER"})
     @Path("/rgb")
-    public Response getImageRGBFromHyper(@RequestBody ImageRGBFromHyper request) throws IOException {
+    public Response getImageRGBFromHyper(@RequestBody ImageRGBFromHyper request, @HeaderParam("userId") int userId) throws IOException {
+        if (userId != imageService.getImageFromId(request.id).getUserId()) {
+            return Response.status(Response.Status.FORBIDDEN).build();
+        }
+
         String filePath = "src/main/resources/Image/Output/" +
                 "hyper_" + request.id + "_" + request.red + "_" + request.green + "_" + request.blue + ".png";
+        Process p = Runtime.getRuntime().exec("python -m http.server 8888");
         if (Files.exists(Paths.get(filePath))) {
             return Response.ok("http://100.96.184.148:8888/src/main/resources/Image/Output/" +
                     "hyper_" + request.id + "_" + request.red + "_" + request.green + "_" + request.blue + ".png").build();
         }
-        Process p = Runtime.getRuntime().exec("python -m http.server 8888");
         return Response.ok(imageService.getImageRGBFromHyper(request)).build();
     }
 
